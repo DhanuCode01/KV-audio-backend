b1fd8d890bdfe4f902cc8e86273c8be17e1c926d
DhanuCode01 <lakshanshanaka327@gmail.com>
2025-02-05 14:51:53 +0530

    update inquiryController,InquiryRouter( get Inquiries and it' Router, delete Inquiries and it' Router)😎

--- Controller/InquiryController.js
+++ Controller/InquiryController.js
@@ -1,8 +1,8 @@
 import Inquiry from "../Models/Inquiry.js";
-import { isItCustomer } from "../Validation/UserValidation.js";
+import { isItAdmin, isItCustomer } from "../Validation/UserValidation.js";
 import {isToken} from "../Validation/TokenValidation.js"
 
-export async function addInquiry (req,res){
+export async function addInquiry (req,res){         //add inquiry
     try {
           isToken(req,res);//if you have a token
         if(isItCustomer(req)){
@@ -37,4 +37,82 @@ export async function addInquiry (req,res){
            error:"database connection un successfully"})
     }
         
+}
+
+
+export async function getInquiriry(req,res){     //viwe inquiry        //Shows the user only their inquiries. //Shows everything to admin.
+    
+    try{
+
+    isToken(req,res);//if you have a token  
+    if(isItCustomer(req)){
+        const inquiries =await Inquiry.find({email:req.user.email}); //Check the email for inquiries.
+        res.status(200).json(inquiries);
+
+        return;
+    }else if (isItAdmin(req)){
+        const inquiries =await Inquiry.find(); //Check the email for inquiries.
+        res.status(200).json(inquiries);
+
+        return;
+    } 
+    
+}catch(error){                                                      //If the lines are not running, it is a connection error.
+    res.status(500).json({
+       error:"database connection un successfully"})
+}
+
+}
+
+
+export async function deleteInquiry(req,res) {              //delete the user only their inquiries. //delete everything to admin.
+    try {
+
+        isToken(req,res);//if you have a token
+        
+        if(isItAdmin(req)){
+            await Inquiry.deleteOne({id:req.params.id}); // Check if the parameter id is the same as the database id
+            res.status(200).json({
+                message:"Inquiry delete Successfully"
+            });
+
+            return;
+        
+        }else if(isItCustomer(req)){
+            const inquiries=await Inquiry.findOne({id:req.params.id}); // Check if the parameter id is the same as the database id
+            
+            if(inquiries==null){                                        //have not inquiries or have inquiries
+                res.status(404).json({
+                    message:"Inquiry not found"
+                })
+                return;
+            }else{
+                if(inquiries.email == req.user.email){
+
+                    await Inquiry.deleteOne({id:req.params.id});        
+                    res.status(200).json({
+                    message:"Inquiry delete Successfully"
+                });
+                return;
+
+                }else{
+                    res.status(403).json({
+                        Message:"your are not authorized to perform this acction"   
+                    })
+                    return
+
+                }
+
+            }
+                
+
+        }
+
+        
+    }catch(error){                                                      //If the lines are not running, it is a connection error.
+        res.status(500).json({
+           error:"database connection un successfully"})
+    }
+    
+    
 }
